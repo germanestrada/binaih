195ddc1a11161098792ee92621e3bfe36c108d80
German Ricardo Estrada <german@iMac-de-German.local>
2026-05-23 21:39:03 -0500

    fix: remover __dirname de prisma.config, agregar .vercelignore

--- prisma.config.ts
+++ prisma.config.ts
@@ -3,6 +3,6 @@ import { defineConfig } from 'prisma/config'
 export default defineConfig({
   schema: 'prisma/schema.prisma',
   datasource: {
-    url: 'postgresql://postgres.njendauzjikgbguowaah:G2rm1n7.777@aws-1-sa-east-1.pooler.supabase.com:6543/postgres',
+    url: process.env.DIRECT_URL ?? process.env.DATABASE_URL,
   },
 })
